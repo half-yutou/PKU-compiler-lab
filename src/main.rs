3b6e90353a570808fad068fa1aec377c5fc932c7
makotoxu <3369726918@qq.com>
2025-08-20 16:11:27 +0800

    lab2 passed

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ use lalrpop_util::lalrpop_mod;
 use std::env::args;
 use std::fs::read_to_string;
 use std::io::Result;
+use koopa::ir::Program;
+use pku_compiler::lab1;
 
 // 引用 lalrpop 生成的解析器
 // 因为我们刚刚创建了 sysy.lalrpop, 所以模块名是 sysy
 lalrpop_mod!(sysy);
+
+const MODE_KOOPA: &str = "-koopa";
+const MODE_RISCV: &str = "-riscv";
 
 fn main() -> Result<()> {
     // 解析命令行参数
@@ -21,21 +26,31 @@ fn main() -> Result<()> {
 
     // 调用 lalrpop 生成的 parser 解析输入文件
     let ast = sysy::CompUnitParser::new().parse(&input).unwrap();
+    let koopa_ir_in_memory = lab1::gen::generate_koopa_ir(ast);
 
-    // 输出解析得到的 AST
-    // println!("{:#?}", ast);
-    // println!("============");
-    // 上述ast是内存中的数据结构，我们需要将其转换为koopa ir内存形式
-    let koopa_ir_in_memory = pku_compiler::generate_koopa_ir(ast);
-    // let koopa_ir_in_memory = pku_compiler::fib_koopa_ir();
-    // 转换为文本形式
+    if mode == MODE_KOOPA {
+        output_koopa_ir(koopa_ir_in_memory, &output)?;
+    } else if mode == MODE_RISCV {
+        output_riscv_assembly(koopa_ir_in_memory, &output)?;
+    } else {
+        panic!("invalid mode");
+    }
+
+    Ok(())
+}
+
+// 输出koopa ir文本到指定文件
+fn output_koopa_ir(koopa_ir_in_memory: Program, output_file: &str) -> Result<()> {
     let mut generator = koopa::back::KoopaGenerator::new(Vec::new());
     generator.generate_on(&koopa_ir_in_memory)?;
     let koopa_ir_text = std::str::from_utf8(&generator.writer()).unwrap().to_string();
-    // 输出koopa ir文本
-    println!("{}", koopa_ir_text);
-    // 写入输出文件
-    std::fs::write(output, koopa_ir_text)?;
+    std::fs::write(output_file, koopa_ir_text)?;
+    Ok(())
+}
 
+// 输出risc-v汇编到指定文件
+fn output_riscv_assembly(koopa_ir_in_memory: Program, output_file: &str) -> Result<()> {
+    let riscv_assembly_text = pku_compiler::lab2::gen::generate_riskv_assembly(koopa_ir_in_memory);
+    std::fs::write(output_file, riscv_assembly_text)?;
     Ok(())
 }
